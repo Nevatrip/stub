32ac326ce7f3e34f488c5ec4ce793a83a0a1e9b0
Realetive <realetive@yandex.ru>
2018-09-26 11:38:41 +0300

    Add tour to filter's result

--- common.blocks/filter/__result/filter__result.bemhtml.js
+++ common.blocks/filter/__result/filter__result.bemhtml.js
@@ -6,7 +6,21 @@ block('filter').elem('result')(
             // content: node._result.map(item=>({
             content: {
                 block: 'tour',
-                mods: {'preview': true},
+                mods: { view: 'preview' },
+                tour: {
+                    name: 'Название экскурсии',
+                    img: 'https://nevatrip.ru/assets/img/e/meteors/petergof.jpg',
+                    features: [
+                        'Unde deleniti deserunt fuga perspiciatis mollitia cum, dicta expedita natus debitis…',
+                        'Harum a totam reiciendis vel, aspernatur numquam mollitia asperiores tenetur accusantium…',
+                        'Labore magnam expedita iste sunt, atque a earum nemo voluptas fuga…',
+                        'Consequuntur quam cum, odio tempore consequatur deserunt repellat eveniet totam accusantium…',
+                        'Quam est numquam soluta quos dolorem non, laudantium, voluptates assumenda…',
+                    ],
+                    price: 100500,
+                    pricePierce: 100500,
+                    urlBuy: 'https://nevatrip.ru/meteors/petergof'
+                }
             }
             // }))
         }
